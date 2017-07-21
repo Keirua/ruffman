15f75b25d14d6f1186baf0532010dc658feb175a
clement <clement@keiruaprod.fr>
2017-07-21 10:31:08 +0200

    added the build_count function

---
+++ README.md
@@ -0,0 +1,9 @@
+# README
+
+Implementation of the [huffman coding](https://en.wikipedia.org/wiki/Huffman_coding) compression algorithm in Rust
+
+# Setup
+
+Install the git pre-commit hook in order to run the unit tests before a commit:
+
+  $ ln -s "$(pwd)/pre-commit.sh" .git/hooks/pre-commit
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,29 @@
 extern crate ruffman;
 
+use std::collections::HashMap;
+
+/// Returns a HashTable whose keys are the characters in the original string, and whose values
+/// is the number of times it appears in the string
+/// ex: "abbcccc" -> {'c': 4, 'b': 2, 'a': 1}
+fn count_chars(original: &String) -> HashMap<char, i32> {
+    let mut map: HashMap<char, i32> = HashMap::new();
+    for key in original.chars() {
+        let count = map.entry(key).or_insert(0);
+        *count += 1;
+    }
+
+    map
+}
+
 fn main() {
     let original = "abbcccc";
     let mut packer = ruffman::BitPacker::new();
-    // packer.pack_bit(1);
-    // packer.pack_bit(0);
-    // packer.pack_bit(1);
-    // packer.pack_bit(1);
 
     let bits = vec![1,0,0,0,0,0,0,1,1];
     packer.pack_bits(&bits);
-    println!("original: {}", original);
+
+    let hash = count_chars(&String::from(original));
+    println!("original: {}", &original);
     println!("{:?}", packer);
+    println!("{:?}", hash);
 }
